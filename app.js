d621739b725f44b76f01d5f5c1e62eee97b17b22
Anand014 <kumar.anand014@gmail.com>
2020-08-20 17:48:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,250 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const crypto = require("crypto");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const session = require('express-session');
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(session({
+    secret: "level5ofsecurityiscool.",
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+// UPLOAD IMAGE IN DATABASE------>
+
+// const mongoURI = 'mongodb://localhost:27017/elearnDB';
+
+// const conn = mongoose.createConnection(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true });
+
+// // Init gfs
+// let gfs;
+
+// conn.once("open", () => {
+//     // Init stream
+//     gfs = Grid(conn.db, mongoose.mongo);
+//     gfs.collection("uploads");
+// });
+
+// // create storage engine
+// const storage = new GridFsStorage({
+//     url: mongoURI,
+//     file: (req, file) => {
+//       return new Promise((resolve, reject) => {
+//         crypto.randomBytes(16, (err, buf) => {
+//           if (err) {
+//             return reject(err);
+//           }
+//           const filename = buf.toString('hex') + path.extname(file.originalname);
+//           const fileInfo = {
+//             filename: filename,
+//             bucketName: 'uploads'
+//           };
+//           resolve(fileInfo);
+//         });
+//       });
+//     }
+//   });
+//   const upload = multer({storage});
+
+
+mongoose.connect("mongodb+srv://Anand:12345@yourblogdb.lhdpi.mongodb.net/blogDB",{ useUnifiedTopology: true, useNewUrlParser: true });
+
+mongoose.set('useCreateIndex', true);
+
+const courseSchema = {
+    name: String,
+    image: String,
+  };
+  
+const Course = mongoose.model("Course", courseSchema);
+
+const userSchema = new mongoose.Schema ({
+    name: String,
+    email: String,
+    password: String,
+    googleId: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  });
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login",isLoggedOut, function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/home", function(req, res){
+    res.redirect("/");
+});
+
+app.get("/courses", function(req, res){
+    Course.find({}, function(err, allcourses){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("courses", {courses:allcourses});
+        }
+    });
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/admin", function(req, res){
+    res.render("admin");
+});
+
+app.post("/admin", function(req, res){
+    const adminPassword = process.env.ADMIN_PASSWORD;
+    const checkadminPassword = req.body.verify
+
+
+    if ( checkadminPassword === adminPassword ) {
+        res.render("newCourse");
+    } else {
+        res.redirect("admin");
+    } 
+});
+
+
+
+app.post("/courses", function(req, res){
+    const name = req.body.name;
+    const image = req.body.image;
+    const newCourse = {name: name, image: image}
+    
+    Course.create(newCourse, function(err, newlycreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/courses");
+        }
+    });
+    
+});
+
+app.post("/register", function(req, res){
+
+    User.register({name: req.body.name, username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            });
+        }
+    });
+});
+
+
+app.post("/login", function(req, res){
+    const user = new User({
+        name: req.body.name,
+        username:  req.body.username,
+        password : req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            });
+        }
+    })
+});
+
+app.get("/logout", function(req, res){
+    req.logOut();
+    res.redirect("/");
+});
+
+
+
+
+//Middleware --> Checking user is authenticated or not
+function isLoggedOut(req, res, next){
+    if(!req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/");
+}
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+    console.log("The server has started at port 3000.");
+  });
