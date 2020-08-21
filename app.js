ffb4e39c971df64fea393f3c0ac3a1da133fed2c
Anand014 <kumar.anand014@gmail.com>
2020-08-21 18:43:04 +0530

    courseinfo

--- app.js
+++ app.js
@@ -73,14 +73,26 @@ app.use(function(req, res, next){
 //   });
 //   const upload = multer({storage});
 
+mongoose.connect("mongodb://localhost:27017/elearnDB",{ useUnifiedTopology: true, useNewUrlParser: true } );
 
-mongoose.connect("mongodb+srv://Anand:12345@yourblogdb.lhdpi.mongodb.net/blogDB",{ useUnifiedTopology: true, useNewUrlParser: true });
+// mongoose.connect("mongodb+srv://Anand:12345@yourblogdb.lhdpi.mongodb.net/blogDB",{ useUnifiedTopology: true, useNewUrlParser: true });
 
 mongoose.set('useCreateIndex', true);
 
 const courseSchema = {
     name: String,
     image: String,
+    tablehead1: String,
+    tablehead2: String,
+    tablebody1: String,
+    tablebody11 : String,
+    tablebody12 : String,
+    tablebody13 : String,
+    tablebody2: String,
+    tablebody21: String,
+    tablebody22: String,
+    tablebody23: String,
+    price: String,
   };
   
 const Course = mongoose.model("Course", courseSchema);
@@ -158,6 +170,7 @@ app.get("/courses", function(req, res){
     });
 });
 
+
 app.get("/about", function(req, res){
     res.render("about");
 });
@@ -183,7 +196,33 @@ app.post("/admin", function(req, res){
 app.post("/courses", function(req, res){
     const name = req.body.name;
     const image = req.body.image;
-    const newCourse = {name: name, image: image}
+    const tablehead1 = req.body.tablehead1;
+    const tablehead2 = req.body.tablehead2;
+    const tablebody1 = req.body.tablebody1;
+    const tablebody11 = req.body.tablebody11;
+    const tablebody12 = req.body.tablebody12;
+    const tablebody13 = req.body.tablebody13;
+    const tablebody2 = req.body.tablebody2;
+    const tablebody21 = req.body.tablebody21;
+    const tablebody22 = req.body.tablebody22;
+    const tablebody23 = req.body.tablebody23;
+    const price = req.body.price;
+
+    const newCourse = {
+        name: name,
+        image: image,
+        tablehead1: tablehead1,
+        tablehead2: tablehead2,
+        tablebody1: tablebody1,
+        tablebody11: tablebody11,
+        tablebody12: tablebody12,
+        tablebody13: tablebody13,
+        tablebody2: tablebody2,
+        tablebody21: tablebody21,
+        tablebody22: tablebody22,
+        tablebody23: tablebody23,
+        price: price
+    }
     
     Course.create(newCourse, function(err, newlycreated){
         if(err){
@@ -194,6 +233,31 @@ app.post("/courses", function(req, res){
     });
     
 });
+
+
+app.get("/courses/:courseId", function(req, res){
+
+    const requestedCourseId = req.params.courseId;
+    
+      Course.findOne({_id: requestedCourseId}, function(err, course){
+        res.render("courseInfo", {
+            name : course.name,
+            image : course.image, 
+            tablehead1 : course.tablehead1,
+            tablehead2 : course.tablehead2, 
+            tablebody1 : course.tablebody1,
+            tablebody2 : course.tablebody2, 
+            tablebody11 : course.tablebody11,
+            tablebody21 : course.tablebody21, 
+            tablebody12 : course.tablebody12,
+            tablebody22 : course.tablebody22, 
+            tablebody13 : course.tablebody13,
+            tablebody23 : course.tablebody23,
+            price : course.price
+        });
+      });
+    });
+    
 
 app.post("/register", function(req, res){
 
